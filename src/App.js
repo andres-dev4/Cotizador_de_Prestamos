58373ec12f252e885455de48588268e0ff5e33eb
Antonio_4 <andresl@finerioconnect.com>
2022-10-13 12:45:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./componentes/Header";
+import { useState } from "react";
+import Formulario from "./componentes/Formulario";
 
 function App() {
+  const [cantidad,guardarCantidad] = useState(0);
+  
+  const [plazo,guardarPlazo] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        titulo="Cotizador de prestamos"
+      />
+      <div className="container">
+        <Formulario
+        cantidad={cantidad}
+        guardarCantidad={guardarCantidad}
+        plazo={plazo}
+        guardarPlazo={guardarPlazo}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/componentes/Header.js
@@ -0,0 +1,17 @@
+
+// function Header({titulo}){
+//     return(
+//         <h1>{titulo}</h1>
+//     )
+// }
+
+const Header = ({titulo}) => {  
+    return(
+        <>        
+        <h1>{titulo}</h1>
+        </>   
+    )
+}
+ 
+
+export default Header;
